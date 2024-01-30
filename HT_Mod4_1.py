703c19f5fc1df32b1b123cebece4b5257e273d1a
IgorIlnicki <symbol_t@ukr.net>
2024-01-30 15:29:00 +0200

    A3

--- HT_Mod4_1.py
+++ HT_Mod4_1.py
@@ -1,12 +1,32 @@
 from pathlib import Path
+import re
 
 def total_salary(path):
-   with open(path, 'r') as fh:
-      print(f" fh = {fh}")
+    total = 0
+    with open(path, 'r', encoding="utf-8") as files:
+        fh =str(files.read()) # перетворюємо текст в рядок
+        # print(type(fh))
+        try:  # перевіряємо 
+            p1 = r"\d+" 
+            fh1 = ' '.join(re.findall(p1,fh)) 
+            # print(f" fh1 = {fh1}")
+            fh2 = fh1.split() # розділяємо рядок на елементи
+            # print(f" fh2 = {fh2}")
+            # print(len(fh2))
+            for element in fh2:
+                element = int(element)
+                # print(f" element = {element}")
+                total = total + element 
+            average = total/len(fh2)
+            print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+        except Exception as error:  # якщ
+            print(f"Зчитування файлу неможливе: {error}")
 
 path = Path("salary_file.txt")
-with open(path, 'r') as fh:
-      fh.
-      print(f" fh = {fh}")
+try: # перевіряємо на наявність файлу
+    total_salary(path)
+except Exception as error: 
+    print(f"Зчитування файлу неможливе: {error}")
 
-total_salary(path)
+
