f93648783cc701ee3a08d4ca87bf2442deeb5232
IgorIlnicki <symbol_t@ukr.net>
2024-01-30 13:47:50 +0200

    A7

---
+++ HT_Mod4_1.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+def total_salary(path):
+   with open(path, 'r') as fh:
+      print(f" fh = {fh}")
+
+path = Path("salary_file.txt")
+with open(path, 'r') as fh:
+      fh.
+      print(f" fh = {fh}")
+
+total_salary(path)
