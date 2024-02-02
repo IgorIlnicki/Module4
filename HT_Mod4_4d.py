bcff02b452a11a6af44fed69626be081c6a74987
IgorIlnicki <symbol_t@ukr.net>
2024-02-02 13:04:05 +0200

    A8

--- HT_Mod4_4d.py
+++ HT_Mod4_4d.py
@@ -6,27 +6,60 @@ def parse_input(user_input):
     # print(f" cmd = {cmd} *args = {args}")
     return cmd, *args
 
+# def add_contact(args, contacts):
+#     name, phone = args
+#     contacts[name] = phone
+#     return print(f"Contact {name} added phone: {contacts[name]}") 
+
 def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
-    return print(f"Contact {name} added phone: {contacts[name]}") 
+    flag = True # флаг контролю знаходження імені в БД
+    file = open('Contacts_phone.txt', "rt")
+    for s in file:
+        s = s.rstrip()  # Забираємо останній символ '\n' з s
+        # print("s = ", s) # Вивести s для контролю
+        contacts = s.split(":")
+        if name == contacts[0]: 
+            flag = False   # підтвердження знаходження імені в БД
+            print(f"Error: phone {name} set incorrectly: this name already exists in the database") # 3 
+            break 
+    if flag:
+        if str(phone).isdigit(): # перевірка телефону - чи число
+            print(f"Contact {name} added phone: {int(phone)}") 
+            file = open('Contacts_phone.txt', "a") # Відкриваємо текстовий файл для запису та дозапису
+            # Сформуємо рядок виду key:value
+            s = str(name) # взяти ключ як рядок
+            s += ':' # додати символ ':'
+            s += str(phone) # додати значення value
+            s += '\n' # додати символ нового рядка
+            print(f" s = {s}")
+            file.write(s) 
+            file.close()
+        else:   # 2
+            print(f"Error: phone {name} set incorrectly: is not numbers") # 3 
+    return
 
 def change_to_file(args, contacts):
+    flag = False # флаг контролю знаходження імені в БД
     file = open('Contacts_phone.txt', "rt")
     ss = str() # створюємо пустий список
     for s in file:
         s = s.rstrip()  # Забираємо останній символ '\n' з s
-        # print("s = ", s) Вивести s для контролю
+        # print("s = ", s) # Вивести s для контролю
         contacts = s.split(":")
         if args[0] == contacts[0]: 
+            flag = True   # підтвердження знаходження імені в БД
             contacts[1] = args[1]
-            print(f" name: {contacts[0]} new (change) phone: {contacts[1]}") 
-            s = str(contacts[0]) + ":" + str(contacts[1])
-        else:
-            print(f"Name {args[0]} is absent")
-            break
+            if str(contacts[1]).isdigit(): # перевірка телефону - чи число 1
+                print(f"Name: {contacts[0]} new (change) phone: {contacts[1]}") 
+                s = str(contacts[0]) + ":" + str(contacts[1])
+            else:   # 2
+                print(f"Error: phone {contacts[0]} set incorrectly: is not numbers") # 3 
         ss += s # додаємо до нашого списку рядок з іменем та телефоном
         ss += '\n' # додати символ нового рядка
+    if not flag:
+        print(f"Name {args[0]} is absent")
     file.close()
     file = open('Contacts_phone.txt', "wt")
     file.write(ss)
@@ -35,7 +68,8 @@ def change_to_file(args, contacts):
 
 def show_phone(args, contacts):
     name  = args[0]
-    print(f"show_phone name: {name}")
+    flag = False
+    # print(f"show_phone name: {name}")
     file = open('Contacts_phone.txt', "rt")
     contacts = {} # Створити пустий словник
     for lines in file: # Використовуєм ітератор файлу
@@ -44,13 +78,14 @@ def show_phone(args, contacts):
         key = strings[0] # отримуємо ключ
         # print(f"key {key} name {name}")
         if key == name:
-            value = strings[1].rstrip() # отримуємо значення без '\n'
-            print(f"Phone {key} is: {value}") 
-            contacts[key] = value # додаємо телефон до словника
-            break
-        else:
-            print(f"Name {key} is absent")
-    # print("contacts = ", contacts)  Вивести словник для контролю
+                value = strings[1].rstrip() # отримуємо значення без '\n'
+                print(f"Phone {key} is: {value}") 
+                contacts[key] = value # додаємо телефон до словника
+                flag = True
+                break
+    if not flag:
+        print(f"Name {name} is absent")
+    # print("contacts = ", contacts) # Вивести словник для контролю
     file.close()
     return contacts
 
@@ -66,18 +101,18 @@ def show_all(contacts):
     file.close()
 
 
-def write_to_file(r1):
-    # print(f" r1 = {r1}")
-    # Відкриваємо текстовий файл для запису та дозапису
-    file = open('Contacts_phone.txt', "a")
-    # Сформуємо рядок виду key:value
-    s = str(r1[0]) # взяти ключ як рядок
-    s += ':' # додати символ ':'
-    s += str(r1[1]) # додати значення value
-    s += '\n' # додати символ нового рядка
-    # print(f" s = {s}")
-    file.write(s) 
-    file.close()
+# def write_to_file(args):
+#     # print(f" r1 = {r1}")
+#     # Відкриваємо текстовий файл для запису та дозапису
+#     file = open('Contacts_phone.txt', "a")
+#     # Сформуємо рядок виду key:value
+#     s = str(r1[0]) # взяти ключ як рядок
+#     s += ':' # додати символ ':'
+#     s += str(r1[1]) # додати значення value
+#     s += '\n' # додати символ нового рядка
+#     # print(f" s = {s}")
+#     file.write(s) 
+#     file.close()
 
 def main():
     contacts = {}  #  словник з іменем і номером телефону
@@ -95,13 +130,13 @@ def main():
 
         elif command == "add":
             add_contact(args, contacts)
-            write_to_file(args)
+            # write_to_file(args)
 
         elif command == "change":
             change_to_file(args, contacts)
 
         elif command == "phone":
-            print(show_phone(args, contacts))
+            show_phone(args, contacts)
 
         elif command == "all":
             show_all(contacts)
